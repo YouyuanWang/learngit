a2a3d27491308637cd7dec07fce1fe17588204c7
Anakin Wang <wangyouyuan@sjtu.edu.cn>
2017-04-15 16:00:44 +0800

    Add files via upload

--- draw_trend.py
+++ draw_trend.py
@@ -9,15 +9,15 @@ with open('year_papernumber.txt') as f:
     for line in lines:
         num = filter(str.isdigit,line)
         paper_num.append(int(num))
+year = range(1996,2016)
 min_num = min(paper_num)
 max_num = max(paper_num)
-year = range(1996,2016)
 #print len(year),len(paper_num)
 plt.plot(year,paper_num,'-')
 plt.xlabel('Year(1996-2016)',fontsize=15)
 plt.ylabel('Number of Paper(Conference,Patent) Published',fontsize=10)
 plt.title('the Research Trend in nearly 20 years',fontsize=15,color='g')
-plt.axis([1994,2017,16000,21000])
+plt.axis([1994,2017,min_num*0.95,max_num*1.05])
 plt.savefig('ResearchTrend.png')
 plt.show()
 
